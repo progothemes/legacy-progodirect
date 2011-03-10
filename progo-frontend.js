ab1430caf289481d0feca716b79d9afdd385f783
alex chousmith <alex@ninthlink.com>
2011-03-10 00:42:36 +0000

    more cleanup of MWP and progo.com/direct

--- progo-frontend.js
+++ progo-frontend.js
@@ -1,8 +1,24 @@
 jQuery(function($) {
 	//alert('oyea');
-	$('#primary form.pform label').addClass('jq');
-	
-	Cufon.replace('#content h2', { fontFamily: 'TitilliumText' });
+	$('#side form.pform label').each(function() {
+		$(this).addClass('jq');
+		if($(this).next().is('select')) {
+			$(this).hide().next().children('option[value=""]').text($(this).html());
+		} else {
+			$(this).next().bind({
+				focus: function() {
+					$(this).prev().hide();
+				},
+				blur: function() {
+					if($(this).val()=='') {
+						$(this).prev().show();
+					}
+				}
+			});
+		}
+	});
+	Cufon.replace('#main h2, #slogan', { fontFamily: 'TitilliumText' });
+	Cufon.replace('#arrow span', { fontFamily: 'TitilliumText', textShadow: '-1px -1px rgba(0, 0, 0, 0.65)' });
 	Cufon.now();
 
 });
