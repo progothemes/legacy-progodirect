26b5737e75bf282b2fb11155f7a9782c40e1456d
alex chousmith <alex@ninthlink.com>
2011-03-23 20:01:56 +0000

    after further updates adding more hooks and filters and dynamic goodness, lets call this 1.0.60

--- functions.php
+++ functions.php
@@ -7,7 +7,9 @@ function progodirect_setup() {
 	add_action( 'init', 'progodirect_init' );
 	add_action( 'progo_frontend_scripts', 'progodirect_scripts', 20 );
 	add_action( 'progo_direct_after_arrow', 'progodirect_contenttop', 20 );
+	add_action( 'progo_pre_gateways', 'progodirect_gatewaycleanup' );
 	add_filter( 'progo_display_easysecure', 'progodirect_easyoverride', 10, 3);
+	add_filter( 'progo_checkout_btn', 'progodirect_checkoutbtn' );
 }
 
 function progodirect_init() {
@@ -66,6 +68,11 @@ function progo_direct_submitbtn( $pid=0, $btxt = 'BUY NOW' ) {
 	return '<input type="submit" id="product_'. absint($pid) .'_submit_button" name="Buy" value="'. esc_html($btxt) .'" class="buynow sbtn" />';
 }
 
+function progodirect_checkoutbtn( $html ) {
+	//<input type='submit' value='$options[button]' name='submit' class='make_purchase wpsc_buy_button sbtn buynow' />
+	return '<input type="image" name="submit" value="submit" class="make_purchase wpsc_buy_button buynow sbtn img" src="'. dirname(get_stylesheet_uri()) .'/images/download.jpg" />';
+}
+
 function progo_sitelogo() {
 	$options = get_option( 'progo_options' );
 	$progo_logo = $options['logo'];
@@ -86,4 +93,44 @@ function progodirect_easyoverride($content, $args, $instance) {
 	$content .= $before_widget . $before_title .'Talk with a Pro'. $after_title;
 	$content .= '<a href="#contact">Contact ProGo Themes</a>';
 	return $content;
+}
+
+function progodirect_gatewaycleanup() {
+	// custoom cleanup of Paypal Pro gateway fields...
+	$years = $months = '';
+	$curryear = date( 'Y' );
+	//generate year options
+	for ( $i = 0; $i < 10; $i++ ) {
+		$years .= "<option value='" . $curryear . "'>" . $curryear . "</option>\r\n";
+		$curryear++;
+	}
+	$oot = "<tr><td><label>" . __( 'Card Type *', 'wpsc' ) . "</label><select class='wpsc_ccBox' name='cctype'>
+			<option value='Visa'>" . __( 'Visa', 'wpsc' ) . "</option>
+			<option value='Mastercard'>" . __( 'MasterCard', 'wpsc' ) . "</option>
+			<option value='Discover'>" . __( 'Discover', 'wpsc' ) . "</option>
+			<option value='Amex'>" . __( 'Amex', 'wpsc' ) . "</option>
+		</select><label>" . __( 'Card Number *', 'wpsc' ) . "</label><input type='text' value='' name='card_number' class='text txt req' />
+		<label>" . __( 'Expiration *', 'wpsc' ) . "</label>
+		<select class='wpsc_ccBox' name='expiry[month]'>
+			" . $months . "
+			<option value='01'>01</option>
+			<option value='02'>02</option>
+			<option value='03'>03</option>
+			<option value='04'>04</option>
+			<option value='05'>05</option>						
+			<option value='06'>06</option>						
+			<option value='07'>07</option>					
+			<option value='08'>08</option>						
+			<option value='09'>09</option>						
+			<option value='10'>10</option>						
+			<option value='11'>11</option>																			
+			<option value='12'>12</option>																			
+			</select><select class='wpsc_ccBox' name='expiry[year]'>
+			" . $years . "
+			</select>
+		<label>" . __( 'CVV *', 'wpsc' ) . "</label><input type='text' size='4' value='' maxlength='4' name='card_code' class='txt text req cvv' />
+		</td>
+	</tr>";
+	global $gateway_checkout_form_fields;
+	$gateway_checkout_form_fields[wpsc_merchant_paypal_pro] = $oot;
 }
--- progo-frontend.js
+++ progo-frontend.js
@@ -22,6 +22,7 @@ jQuery(function($) {
 	Cufon.replace('#main h2, #slogan', { fontFamily: 'TitilliumText' });
 	Cufon.replace('#arrow span', { fontFamily: 'TitilliumText', textShadow: '-1px -1px rgba(0, 0, 0, 0.65)' });
 	Cufon.replace('#secondary h3', { fontFamily: 'TitilliumText', fontWeight: '800' });
+	Cufon.replace('h1.page-title', { fontFamily: 'TitilliumText', color: '-linear-gradient(#f69629, #c15400' });
 	Cufon.now();
 
 });
